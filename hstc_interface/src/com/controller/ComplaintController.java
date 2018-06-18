940866db09dda9e27298728735b1a71e050b72fe
DESKTOP-BELVMNP\yy <596257180@qq.com>
2018-06-19 00:25:10 +0800

    seventh commit

--- hstc_interface/src/com/controller/ComplaintController.java
+++ hstc_interface/src/com/controller/ComplaintController.java
@@ -8,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pojo.TComplaint;
+import com.pojo.TRecruitment;
 import com.service.ComplaintService;
+import com.service.RecruitmentService;
 import com.utils.IDUtils;
 
 @Controller
 public class ComplaintController {
 	@Autowired
 	ComplaintService complaintService;
+	@Autowired
+	RecruitmentService recruitmentService;
+	
 	
 	@RequestMapping(value="/insertComplaint")
 	@ResponseBody
@@ -25,6 +30,17 @@ public class ComplaintController {
 		tComplaint.setId(id);
 		tComplaint.setOpenid(openid);
 		complaintService.insertComplaint(tComplaint);
+		
+		TRecruitment tr1 = recruitmentService.queryById(id);
+		String count1 = tr1.getComplaintCount();
+		int count1_int = Integer.parseInt(count1);
+		String count2 = Integer.toString(count1_int+1);
+		
+		TRecruitment tr2 = new TRecruitment();
+		tr2.setId(id);
+		tr2.setComplaintCount(count2);
+		recruitmentService.edit(tr2);
+		
 		return "true";
 	}
 }
